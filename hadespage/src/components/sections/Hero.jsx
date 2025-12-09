7d08335b8f4205c7f5f4ea777bd6c5090b70cc96
WolveJC <202882572+WolveJC@users.noreply.github.com>
2025-12-09 04:05:09 -0400

    Actualizar Hero.jsx

--- hadespage/src/components/sections/Hero.jsx
+++ hadespage/src/components/sections/Hero.jsx
@@ -5,11 +5,11 @@ function Hero() {
   return (
     <section
       id="presentacion"
-      className="p-10 rounded-xl shadow-lg mb-12 border-4 border-zinc-500 transition-colors duration-300"
+      className="p-10 rounded-xl shadow-lg mb-12 border-4 border-sky-accent transition-colors duration-300"
     >
       <div className="flex flex-col md:flex-row items-center mx-auto max-w-4xl">
         {/* Avatar */}
-        <div className="w-48 h-48 rounded-full overflow-hidden border-4 border-indigo-500 flex-shrink-0 mb-6 md:mb-0 md:mr-10">
+        <div className="w-48 h-48 rounded-full overflow-hidden border-4 border-sky-accent flex-shrink-0 mb-6 md:mb-0 md:mr-10">
           <img
             src={ProfilePic}
             alt="Avatar"
@@ -19,16 +19,17 @@ function Hero() {
 
         {/* Texto de Bienvenida */}
         <div className="text-center md:text-left">
-          <p className="text-xl font-semibold text-indigo-600">
+          <p className="text-xl font-semibold text-sky-accent">
             ¡Hola a todos!
           </p>
-          <h1 className="text-5xl font-extrabold text-gray-900 mt-1 mb-4">
-            Bienvenidos a mi <span className="text-indigo-600">Portafolio</span>
+          <h1 className="text-5xl font-extrabold text-textStage-mid mt-1 mb-4">
+            Bienvenidos a mi{" "}
+            <span className="text-sky-accent">Portafolio</span>
           </h1>
-          <p className="text-lg text-gray-600">
+          <p className="text-lg text-textStage-start">
             Soy desarrollador, ahora mismo enfocado en{" "}
-            <span className="font-bold">React y Tailwind CSS</span>. Explora mis
-            trabajos y descubre cómo puedo ayudar a tu próximo proyecto.
+            <span className="font-bold text-textStage-end">React y Tailwind CSS</span>. 
+            Explora mis trabajos y descubre cómo puedo ayudar a tu próximo proyecto.
           </p>
         </div>
       </div>
